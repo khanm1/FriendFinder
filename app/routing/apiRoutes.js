07bc51d0f7a9deaf36de7200d5dfa55f9bd8c83f
mkhan <riponk9519@gamil.com>
2019-01-14 20:22:49 -0600

    Added more codes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,23 @@
 //Routes
-module.exports = function(app, data){
-  
+module.exports = function (app, data) {
+
     // API GET Requests
-  app.get("/api/friends", function(req, res){
-      return res.json(data);
-  });
-// API POST Requests
-  app.post("/api/friends", function(req, res){
-      var newFriend = req.body;
-      
-      for(var i = 0; i<newFriend.scores.length; i++){
-          var string = newFriend.scores[i];
-          var int = parseInt(string);
-          newFriend.scores.splice(i,1,int);
-      }
-      
-      data.push(newFriend);
-      res.json(newFriend);
-  });
+    app.get("/api/friends", function (req, res) {
+        return res.json(data);
+    });
+    // API POST Requests
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            var string = newFriend.scores[i];
+            var int = parseInt(string);
+            newFriend.scores.splice(i, 1, int);
+        }
+
+        data.push(newFriend);
+        res.json(newFriend);
+    });
 }
 
 
