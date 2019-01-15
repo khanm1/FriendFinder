483c0bfa914106dce72821d02855aee18bf5a857
mkhan <riponk9519@gamil.com>
2019-01-14 18:52:04 -0600

    added codes to server.js file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+// configure our dependencies
 var path = require("path");
 var bodyParser = require("body-parser");
 var express = require("express");
 
+// setting up the express app
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// setting up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -15,8 +18,7 @@ var data = require("./app/data/friends.js");
 require("./app/routing/apiRoutes")(app, data);
 require("./app/routing/htmlRoutes")(app, path);
 
-
-
+// starting the server 
 app.listen(PORT, function(){
     console.log("App listening on PORT" + PORT);
 })
