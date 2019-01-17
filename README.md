080c862c3b8241a5d0cb8078c8f41108eee89129
mkhan <riponk9519@gamil.com>
2019-01-16 21:19:39 -0600

    Added codes and notes

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 A compatibility-based "FriendFinder" application -- basically a dating app for single people to find their perfect match! This full-stack site will take in results from users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
 
 ### Resources Used
-##### HTML
-##### Bootstarp
-##### JavaScript
-##### Node
-##### Express
+* HTML
+* Bootstarp
+* JavaScript
+* Node
+* Express
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,21 @@
 module.exports = [
   {
-      name: "Lolo",
+      name: "Lolo Jones",
       scores: [3, 4, 2, 1, 4, 5, 3, 4, 5, 2],
       image: "https://timedotcom.files.wordpress.com/2015/08/lolo-jones-athlete.jpg?quality=85"
   },
   {
-      name: "Tom",
+      name: "Tom Brady",
       scores: [5, 2, 3, 3, 5, 2, 1, 4, 5, 2],
       image: "https://akns-images.eonline.com/eol_images/Entire_Site/201711/rs_600x600-170201141543-600.tom-brady.cm.2117.jpg?fit=inside|900:auto&output-quality=90"
   },
   {
-      name: "Mia",
+      name: "Mia Hamm",
       scores: [2, 4, 5, 2, 3, 4, 5, 3, 2, 5],
       image: "https://m.media-amazon.com/images/M/MV5BMjEyMzQyNzA0M15BMl5BanBnXkFtZTYwODc3MTA1._V1_UX214_CR0,0,214,317_AL_.jpg"
   },
   {
-      name: "Rob",
+      name: "Rob Gronkowski",
       scores: [4, 3, 4, 1, 4, 5, 2, 1, 2, 3],
       image: "https://m.media-amazon.com/images/M/MV5BNTc2NDA2NTYyOF5BMl5BanBnXkFtZTcwMzU5OTg5Nw@@._V1_.jpg"
   }
